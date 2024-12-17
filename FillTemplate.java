af155d2d6346b55c8752c5c6ca5835f3a939c7f1
William Owen <william.owen.bsc@gmail.com>
2024-12-17 09:36:07 -0800

    Refactor FillTemplate class for improved readability and efficiency; remove unused email template file and update placeholder handling

--- FillTemplate.java
+++ FillTemplate.java
@@ -1,12 +1,5 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Random;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class FillTemplate {
 
@@ -31,20 +24,16 @@ public class FillTemplate {
     public static void processFile(File file)
             throws FileNotFoundException {
         Random numGenerator = new Random();
-            
+
         System.out.println("Processing file: " + file.getName());
-     
+
         Scanner fileScanner = new Scanner(file, "UTF-8");
 
-        // System.out.println("fileScanner" +  fileScanner);
-        // System.out.println("fileScanner.hasNextLine()" +  fileScanner.hasNextLine());
-
-        Queue<Map<String,String>> placeholders = new LinkedList<>();
+        Queue<Map<String, String>> placeholders = new LinkedList<>();
 
         while (fileScanner.hasNextLine()) {
             String templateLine = fileScanner.nextLine();
 
-            // System.out.println("Template line: " + templateLine);
 
             // Replace templates with user responses to prompts
             // Find number of opening tags in line
@@ -61,27 +50,19 @@ public class FillTemplate {
                     }
                 }
             }
-
-            // System.out.println("Processed line: " + templateLine);
-            
-            // Write to output file
-            // outputFile.println(templateLine);
-            
         }
-        // System.out.println("Placeholders: " + placeholders);
 
         // Create a file with the placeholders
         File placeHolderFile = new File("placeholders.txt");
         PrintStream newFileOutput = new PrintStream(placeHolderFile);
-        
-        // create copy of placeholders
-        Queue<Map<String,String>> copyPlaceholders = new LinkedList<>(placeholders);
 
+        // create copy of placeholders
+        Queue<Map<String, String>> copyPlaceholders = new LinkedList<>(placeholders);
 
         while (!copyPlaceholders.isEmpty()) {
             Map<String, String> placeholder = copyPlaceholders.poll();
             for (String key : placeholder.keySet()) {
-            newFileOutput.println(key + ": \n");
+                newFileOutput.println(key + ": \n");
             }
         }
 
@@ -94,7 +75,7 @@ public class FillTemplate {
         }
 
         // Prompt user for responses to Placeholders
-        System.out.println("Please enter responses to the placeholders in the file: " + placeHolderFile);
+        System.out.println("\nPlease enter responses to the placeholders in the file: " + placeHolderFile);
         System.out.println("Press Enter to continue after filling responses");
 
         // check for enter key
@@ -104,13 +85,9 @@ public class FillTemplate {
         newFileOutput.close();
 
         // Read the responses from the file
-        
+
         File fileToRead = new File("placeholders.txt");
         Scanner newFileScanner = new Scanner(fileToRead, "UTF-8");
-
-        // System.out.println("fileToRead: " + fileToRead);
-        // System.out.println("newFileScanner: " + newFileScanner);
-        // System.out.println("newFileScanner.hasNextLine(): " + newFileScanner.hasNextLine());
 
         // Update the linked list to be a map of placeholders and responses
         Map<String, String> combinedPlaceholders = new HashMap<>();
@@ -119,17 +96,16 @@ public class FillTemplate {
             combinedPlaceholders.putAll(map);
         }
 
-
         while (newFileScanner.hasNextLine()) {
             String line = newFileScanner.nextLine();
 
             String prompt = line.substring(0, line.indexOf(">:") + 1);
-            if(prompt.length() == 0) {
+            if (prompt.length() == 0) {
                 continue;
             }
             // print prompt and answer
             // System.out.println("Prompt: " + prompt);
-           
+
             String answer = line.substring(line.indexOf(">:") + 2).trim();
 
             // Set map values in combinedPlaceholders
@@ -139,16 +115,14 @@ public class FillTemplate {
                 }
             }
 
-          
-            // System.out.println("Answer: " + answer);
 
         }
 
-
         // Create output file
-        PrintStream outputFile = new PrintStream(
-                new File(
-                        file.getName().replace(".txt", "--template-filled--" + numGenerator.nextInt(1000000) + "-.txt")));
+        File outputFile = new File(
+                file.getName().replace(".txt",
+                        "--template-filled--" + numGenerator.nextInt(1000000) + "-.txt"));
+        PrintStream outputPrintStream = new PrintStream(outputFile);
 
         // process the file again
         Scanner fileScanner2 = new Scanner(file, "UTF-8");
@@ -156,7 +130,6 @@ public class FillTemplate {
         while (fileScanner2.hasNextLine()) {
             String templateLine = fileScanner2.nextLine();
 
-            // System.out.println("Template line: " + templateLine);
 
             // Replace templates with user responses to prompts
             // Find number of opening tags in line
@@ -166,11 +139,12 @@ public class FillTemplate {
                     for (int j = i + 1; j < templateLine.length(); j++) {
                         if (templateLine.charAt(j) == '>') {
                             String placeholder = templateLine.substring(i, j + 1);
-                            System.out.println("Placeholder: " + placeholder);
-                            System.out.println("combinedPlaceholders: " + combinedPlaceholders);
+                            // System.out.println("Placeholder: " + placeholder);
+                            // System.out.println("combinedPlaceholders: " + combinedPlaceholders);
                             if (combinedPlaceholders.containsKey(placeholder)) {
-                                if(combinedPlaceholders.get(placeholder).length() > 0) {
-                                    templateLine = templateLine.replace(placeholder, combinedPlaceholders.get(placeholder));
+                                if (combinedPlaceholders.get(placeholder).length() > 0) {
+                                    templateLine = templateLine.replace(placeholder,
+                                            combinedPlaceholders.get(placeholder));
                                 }
                             }
                             i = j; // Move the outer loop index to the end of the current placeholder
@@ -180,27 +154,22 @@ public class FillTemplate {
                 }
             }
 
-            // System.out.println("Processed line: " + templateLine);
-
             // Write to output file
-            outputFile.println(templateLine);
+            outputPrintStream.println(templateLine);
 
         }
 
-
-
-        // Write the processed line to the output file
-        // outputFile.println(templateLine);
-
-
-
-
-
         
-
-
-
-        // System.out.println("File processed. Output file created: " + outputFile);
+        // Write the processed line to the output file
+        System.out.println("File processed. Output file created: " + outputFile);
+        
+        
+        // Open up output file in notepad
+        try {
+            Runtime.getRuntime().exec("notepad " + outputFile.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println("Error opening file in notepad: " + e);
+        }
     }
 
     public static File getFile()
